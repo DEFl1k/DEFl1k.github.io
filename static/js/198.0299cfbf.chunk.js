c3b534410f226349bf101b2ff562ad69fbbd04be
DEFL <igrogejmer@gmail.com>
2025-07-09 02:18:22 +0500

    Updates

---
+++ static/js/198.0299cfbf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdeflstream=self.webpackChunkdeflstream||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.0299cfbf.chunk.js.map
